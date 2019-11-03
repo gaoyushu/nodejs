9bd43b4fcfb57de8c4146ca151dcfac313a7b169
gaoyushu <1062945371@qq.com>
2019-11-03 23:09:07 +0800

    task03 new

--- task/task03/task03.js
+++ task/task03/task03.js
@@ -6,39 +6,40 @@ const http = require('http'),
       fs = require('fs');
 
 let usr = [
-  {name:'admin',pswd:'admin'}
+  {name: 'admin', pswd: 'admin'}
 ];
 
 let chapterList = JSON.parse(fs.readFileSync('./js/data.js','utf8'));
 
-// 请求监听（本地路径访问返回NAN）
-http.createServer((req,res)=>{
+// 请求监听
+http.createServer((req, res)=>{
   
-  console.log('==========');
-  console.log('[req msg]', `${req.method} ${req.url} HTTP/${req.httpVersion}`);
+  // console.log('==========');
+  // console.log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
 
   switch(req.method) {
     case 'GET':
-      console.log('METHOD: GET');
+      // console.log('METHOD: GET');
       getfun(req,res);
       // console.log('==========');
       break;
 
     case 'POST':
-      console.log('METHOD: POST')
+      // console.log('METHOD: POST');
       postfun(req,res);
       break;
 
     default:
-      console.log('ERROR METHOD, RETURN ERROR', + req.method);
+      // console.log('ERROR METHOD, RETURN ERROR', + req.method);
       err(res);
       break;
   }
 }).listen(8083);
 
-// GET（改进：减少判断 提高兼容性）
+// GET
 // 跳转+请求数据
-function getfun(req,res){
+// -> chapter.html图片发出的url请求和其他页面不一样
+function getfun(req, res){
   let path = url.parse(req.url);
   let file = __dirname; // D:\GAOYUSHU\task\task03\task
 
@@ -46,18 +47,29 @@ function getfun(req,res){
 
   let pathname1 = path.pathname.split('.'); // 分类型
   let pathname2 = path.pathname.split('/'); // 加路径
-  console.log('----------');
-  console.log(pathname1);
-  console.log(pathname2); 
+  // console.log('----------');
+  // console.log(pathname1);
+  // console.log(pathname2); 
 
-  if(req.url == '/list'){
-    var data = JSON.stringify(chapterList);
-    res.setHeader('Content-Length',Buffer.byteLength(data));
-    res.setHeader('Content-Type','text/plain;charset="utf-8"');
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.end(data);
-  } else if(pathname1.length == 1){
+  if(pathname1.length == 1){
     switch(pathname2[1]){
+      case 'getDetail': 
+        let id = qs.parse(path.query).chapterId;
+        let obj;
+
+        for(let i = 0; i < chapterList.length; i++){
+          if(chapterList[i].chapterId == id){
+            obj = chapterList[i];
+            break;
+          }
+        }
+        
+        res.end(JSON.stringify(obj));
+        break;
+      case 'data':
+        let data = JSON.stringify(chapterList);
+        res.end(data);
+        break;
       case 'list':
         file += '/chapterList.html';
         break;
@@ -73,29 +85,17 @@ function getfun(req,res){
       case 'detail':
         file += '/chapter.html';
         break;
-      case 'getDetail': 
-        let id = qs.parse(path.query).chapterId;
-        let obj;
-
-        for(let i = 0; i < chapterList.length; i++){
-          if(chapterList[i].chapterId == id){
-            obj = chapterList[i];
-            break;
-          }
-        }
-        
-        res.end(JSON.stringify(obj));
-        break;
       default:
-        console.log('ERROR GET!');
-        console.log('PATH.PATHNAME: ' + path.pathname);
+        // console.log('ERROR GET!');
+        // console.log('PATH.PATHNAME: ' + path.pathname);
+        break;
     } 
 
-    console.log('FILE: ' + file);
+    // console.log('FILE: ' + file);
 
-    fs.readFile(file,(err,data)=>{
+    fs.readFile(file,(err, data)=>{
       if(err){
-        console.log(err.message);
+        // console.log(err.message);
         res.end(err.message)
       }
       res.writeHead(200,{
@@ -104,7 +104,7 @@ function getfun(req,res){
       res.end(data);
     });
   }else{
-    console.log('FILE: ' + file);
+    // console.log('FILE: ' + file);
 
     if(pathname2[1] == 'images' || pathname2[1] == 'css' || pathname2[1] == 'js' ){
       file += req.url;
@@ -113,26 +113,26 @@ function getfun(req,res){
     }
     let type = pathname1[pathname1.length-1];
 
-    console.log('FILE: ' + file);
-    console.log('TYPE: ' + type);
+    // console.log('FILE: ' + file);
+    // console.log('TYPE: ' + type);
 
-    fs.readFile(file,(err,data)=>{
+    fs.readFile(file, (err, data)=>{
       if(err){
-        console.log(err.message);
+        // console.log(err.message);
         res.statusCode = 404;
         res.end(err.message);
-      }else{
-        if(type === 'js'|| type === 'html' || type === 'css'){
-          res.writeHead(200,{
-            'Content-Type':'text/' + type
-          })
-        }else{
-          res.writeHead(200,{
-            'Content-Type':'image/' + type
-          })
-        }
-        res.end(data);
       }
+
+      if(type === 'js'|| type === 'html' || type === 'css'){
+        res.writeHead(200, {
+          'Content-Type': 'text/' + type
+        })
+      }else{
+        res.writeHead(200, {
+          'Content-Type': 'image/' + type
+        })
+      }
+      res.end(data);
     });
   }
 }
@@ -148,7 +148,8 @@ function postfun(req, res){
       postadd(req, res);
       break;
     default:
-      console.log('POST ERROR', req.url);
+      // console.log('POST ERROR', req.url);
+      break;
   }
 }
 
@@ -156,7 +157,6 @@ function postfun(req, res){
 // 登录界面 验证身份
 function postlogin(req, res){
   let user = '';
-  let sign = 0;
 
   req.on('data', (data)=>{
     user += data;
@@ -167,15 +167,13 @@ function postlogin(req, res){
 
     usr.map((item)=>{
       if(item.name == user.name && item.pswd == user.pswd){
-        sign = 1;
         res.statusCode = 200;
         res.end('OK');
       }
     });
-    if(sign == 0){
-      res.statusCode = 404;
-      res.end('ERROR')
-    }
+
+    res.statusCode = 401;
+    res.end('ERROR')
 
   });
 }
@@ -184,10 +182,12 @@ function postlogin(req, res){
 // 添加文章
 function postadd(req, res){
   let essay = '';
-  req.on('data',(data)=>{
+
+  req.on('data', (data)=>{
     essay += data;
   });
-  req.on('end',()=>{
+
+  req.on('end', ()=>{
     essay = qs.parse(essay.toString('utf8'));
     let item = {
       chapterId: chapterList.length+1,
@@ -197,10 +197,11 @@ function postadd(req, res){
       chapterContent: essay.content || '',
       publishTimer: new Date().getTime(),
       author: essay.author || undefined,
-      views: essay.views || undefined,
+      views: essay.views || 0,
     }
     chapterList.push(item);
-    fs.writeFileSync('./js/data.js',JSON.stringify(chapterList));
+    // console.log(JSON.stringify(chapterList));
+    fs.writeFileSync('./js/data.js', JSON.stringify(chapterList));
   })
   res.end('OK');
 }
